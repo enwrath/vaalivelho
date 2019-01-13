25678155afa077cd68035c3739b6c1d0b45584c1
Heikki Toivanen <heikki.t.toivanen@aalto.fi>
2019-01-13 15:47:12 +0200

    Votecountersimulator v0.01

---
+++ votecounter/votecounter.js
@@ -0,0 +1,48 @@
+var state = 0; //0 = no vote visible, 1 = vote in middle, 
+var leftImg, rightImg, midImg, currentVote, resultText;
+var votes = [2, 3, 2, 3, 3]; //Randomly create of preset situations?
+var res2 = 2, res3 = 3;
+var vote = -1;
+
+function init() {
+	leftImg = document.getElementById("leftImg");
+    rightImg = document.getElementById("rightImg");
+    midImg = document.getElementById("midImg");
+    currentVote = document.getElementById("currentVote");
+    resultText = document.getElementById("resultText");
+}
+
+function getNextVote() {
+	if (vote < votes.length - 1) {
+		vote++;
+		showVote();
+	}
+	//Change the image when amount of votes left changes to some %
+}
+
+function clicked(number) {
+	console.log(number);
+	if (number == 0 && state == 0) {
+		getNextVote();
+		state = 1;
+	}
+	else if (number == 1 && state == 1) {
+		hideVote();
+		state = 0;
+	}
+}
+
+function showVote() {
+	currentVote.textContent = votes[vote];
+}
+
+function hideVote() {
+	currentVote.textContent = "";
+}
+
+function submitAnswer() {
+	var r2 = document.getElementById("result2").value;
+	var r3 = document.getElementById("result3").value;
+	if (r2 == res2 && r3 == res3) resultText.textContent = "Laskit oikein ja pelastit demokratian!"
+	else resultText.textContent = "Tarkistuslaskussa selvisi laskuissasi tekemäsi virhe. Olet uhka demokratiallemme ja pääset uudelleenkoulutusleirille!";
+}
