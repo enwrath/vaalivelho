7a8041c58b16bd52a18ef1e5be7deadeae2b4db6
Heikki T <hegez1994@gmail.com>
2019-03-07 16:18:45 +0200

    added simple traditional vaalikone

---
+++ vaalikone/kysymyslista.js
@@ -0,0 +1,38 @@
+var questions = [
+	{
+	'text': 'Suomen kansa pitää siirtää halvemman hintatason maihin elinkustannusten madaltamiseksi.',
+	'sdp': 0,
+	'kesk': 0,
+	'kok': 3,
+	'kd': 0,
+	'rkp': 1,
+	'vihr': 2,
+	'vas': 0,
+	'ps': 0,
+	'sin': 0
+	},
+	{
+	'text': 'Suomen pitää siirtää tuotanto ulkomaille päästötavoitteiden saavuttamiseksi.',
+	'sdp': 0,
+	'kesk': 1,
+	'kok': 3,
+	'kd': 1,
+	'rkp': 2,
+	'vihr': 3,
+	'vas': 0,
+	'ps': 0,
+	'sin': 1
+	},
+	{
+	'text': 'Suomen pitää liittyä Natoon.',
+	'sdp': 0,
+	'kesk': 1,
+	'kok': 3,
+	'kd': 1,
+	'rkp': 2,
+	'vihr': 0,
+	'vas': 0,
+	'ps': 1,
+	'sin': 1
+	}
+];
